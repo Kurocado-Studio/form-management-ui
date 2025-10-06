b8667a9e6238ab58baf17adc6d18d7dca92e3fdf
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-10-06 15:01:38 -0500

    fix(config): updates composeFormsNodeTree

--- src/application/store/composeFormsNodeTree.ts
+++ src/application/store/composeFormsNodeTree.ts
@@ -2,37 +2,22 @@ import type {
   Form,
   Section,
 } from '@kurocado-studio/html-form-service-ui-config';
-import { chain, keyBy } from 'lodash-es';
+import { get, keyBy } from 'lodash-es';
 
-import type { FormsNode, FormsNodeTree } from '../../types';
+import type { FormsNodeTree } from '../../types';
 
 export function composeFormsNodeTree(apiForms: Form[] = []): FormsNodeTree {
-  return chain(apiForms)
-    .map((form: Form) => ({
+  return keyBy(
+    apiForms.map((form: Form) => ({
       ...form,
-      sections: chain(form.sections || [])
-        .map((section: Section) => ({
+      sections: keyBy(
+        get(form, ['sections'], []).map((section: Section) => ({
           ...section,
-          questions: keyBy(section.questions || [], 'id'),
-        }))
-        .keyBy('id')
-        .value(),
-    }))
-    .keyBy('id')
-    .value();
-}
-
-export function adaptFormToNodeTree(form: Form): FormsNode {
-  return {
-    [form.id]: {
-      ...form,
-      sections: chain(form.sections)
-        .map((section: Section) => ({
-          ...section,
-          questions: keyBy(section.questions, 'id'),
-        }))
-        .keyBy('id')
-        .value(),
-    },
-  };
+          questions: keyBy(get(section, ['questions'], []), 'id'),
+        })),
+        'id',
+      ),
+    })),
+    'id',
+  );
 }
